a98137a4bcbfcc877f50b7c1f6472870e1e1dc71
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-06-01 19:19:29 +0600

    Starting with Robokassa controller

---
+++ app/controllers/robokassa_controller.rb
@@ -0,0 +1,15 @@
+class RobokassaController < ApplicationController
+
+  def paid
+    
+  end
+
+  def success
+    
+  end
+
+  def fail
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  scope 'robokassa' do
+    match 'paid'    => 'robokassa#paid',    as: :robokassa_paid
+    match 'success' => 'robokassa#success', as: :robokassa_success
+    match 'fail'    => 'robokassa#fail',    as: :robokassa_fail
+  end
 end
