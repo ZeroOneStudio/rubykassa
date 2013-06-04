dc0b51ad13699c41facad0566421136a79d9ad6e
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-06-04 20:40:17 +0600

    setting default configuration values at Configuration constructor

--- lib/rubykassa/configuration.rb
+++ lib/rubykassa/configuration.rb
@@ -1,5 +1,13 @@
 module Rubykassa
   class Configuration
     attr_accessor :login, :first_password, :second_password, :mode, :http_method
+
+    def initialize
+      self.login = "your_login"
+      self.first_password = "first_password"
+      self.second_password = "second_password"
+      self.mode = :test
+      self.http_method = :get
+    end
   end
 end
