e44ec2180b11bd8f6cc36c5bc0f08339ae412f0d
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-06-05 00:28:48 +0600

    set default value for link_to_pay's phrase

--- lib/rubykassa/action_view_extension.rb
+++ lib/rubykassa/action_view_extension.rb
@@ -1,8 +1,8 @@
 module Rubykassa
   module ActionViewExtension
     def link_to_pay phrase, invoice_id, total
+      phrase ||= "Pay"
       total, invoice_id  = total.to_s, invoice_id.to_s
-
       link_to phrase, Rubykassa.pay_url(invoice_id, total, params)
     end
   end
