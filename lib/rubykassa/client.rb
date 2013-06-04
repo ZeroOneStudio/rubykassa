86347f903b1778990c8564a820e756c6d4e40976
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-06-04 20:48:43 +0600

    handling configuration errors when mode or http_method is configured incorrectly

--- lib/rubykassa/client.rb
+++ lib/rubykassa/client.rb
@@ -1,13 +1,17 @@
 require 'rubykassa/configuration'
 
 module Rubykassa
+  class ConfigurationError < StandardError; end
+
   class Client
     class << self
       attr_accessor :configuration
 
       def configure
         self.configuration = Rubykassa::Configuration.new
-        yield self.configuration  
+        yield self.configuration
+        raise ConfigurationError, "Alailable modes are :test or :production" unless [:test, :production].include? self.configuration.mode
+        raise ConfigurationError, "Alailable http methods are :get or :post" unless [:get, :post].include? self.configuration.http_method
       end
     end
   end
--- spec/dummy/config/initializers/rubykassa.rb
+++ spec/dummy/config/initializers/rubykassa.rb
@@ -3,4 +3,5 @@ Rubykassa.configure do |c|
   c.first_password = ENV["ROBOKASSA_FIRST_PASSWORD"]
   c.second_password = ENV["ROBOKASSA_SECOND_PASSWORD"]
   c.mode = :test
+  c.http_method = :get
 end
