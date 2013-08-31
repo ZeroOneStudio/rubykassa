fa280476c72e3673efe8b2efdeccd429b3f05ad2
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-08-31 13:30:11 +0600

    eager load is sret for all environments; wrap parameters disabled

--- spec/dummy/config/initializers/wrap_parameters.rb
+++ spec/dummy/config/initializers/wrap_parameters.rb
@@ -5,9 +5,9 @@
 # is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
-end
+# ActiveSupport.on_load(:action_controller) do
+#   wrap_parameters format: [:json]
+# end
 
 # Disable root element in JSON by default.
 ActiveSupport.on_load(:active_record) do
