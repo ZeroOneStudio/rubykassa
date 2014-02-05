33af154600d6fa54d9e4a3b0f18d334a429d7d09
Sergey Kishenin <sergey.kishenin@gmail.com>
2014-02-05 17:08:33 +0600

    put coveralls to the top of the file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,11 @@
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 
+require 'coveralls'
+Coveralls.wear!
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
 require 'rubykassa'
-require 'coveralls'
-
-Coveralls.wear!
