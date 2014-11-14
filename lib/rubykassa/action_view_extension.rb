a800b438befe41af979cebbf3bb15335c42b67cc
Anton Mur <mibus32@gmail.com>
2014-11-14 22:43:41 +0400

    added block to pay_url

--- lib/rubykassa/action_view_extension.rb
+++ lib/rubykassa/action_view_extension.rb
@@ -1,11 +1,13 @@
 # -*- encoding : utf-8 -*-
 module Rubykassa
   module ActionViewExtension
-    def pay_url phrase = nil, invoice_id, total, options = {}, &block
+    def pay_url(phrase = nil, invoice_id = nil, total = nil, options = {}, &block)
       total, invoice_id  = total.to_s, invoice_id.to_s
+
       extra_params  = options.except([:custom, :html])
       custom_params = options[:custom] ||= {}
       html_params = options[:html] ||= {}
+
       link_to phrase, Rubykassa.pay_url(invoice_id, total, custom_params, extra_params), html_params, &block
     end
   end
