65fd5a8d76a54d3298c02fc823dc89b07c0ffb85
Roman <coolcranky@gmail.com>
2015-02-14 21:18:30 +0300

    To make this more bullet-proof
    I updated to Ruby 2.2 and RoR 4.2 and I have started to appear error when processing "... / robokassa / paid":
    NoMethodError in RobokassaController # paid
    undefined method `downcase 'for nil: NilClass
    
    In this case, something like @params["SignatureValue"] is not defined.
    This converts everything to string to start with. nil.to_s is an empty string by default, so it's safe.

--- lib/rubykassa/notification.rb
+++ lib/rubykassa/notification.rb
@@ -15,7 +15,7 @@ module Rubykassa
 
     %w(result success).map do |kind|
       define_method "valid_#{kind}_signature?" do
-        @params["SignatureValue"].downcase == generate_signature_for(kind.to_sym)
+        @params["SignatureValue"].to_s.downcase == generate_signature_for(kind.to_sym)
       end
     end
 
