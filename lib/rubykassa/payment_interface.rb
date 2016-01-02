034d1b93056b7f9bfadcc39d573d1715b2a6cee0
Dan K <itsdanya@gmail.com>
2016-01-02 22:18:55 +0300

    Update base URL, add IsTest param

--- lib/rubykassa/payment_interface.rb
+++ lib/rubykassa/payment_interface.rb
@@ -1,19 +1,20 @@
 # -*- encoding : utf-8 -*-
 require 'rubykassa/signature_generator'
 
-module Rubykassa  
+module Rubykassa
   class PaymentInterface
     include SignatureGenerator
 
     PARAMS_CONFORMITY = {
-      login:       "MrchLogin",
+      login:       "MerchantLogin",
       total:       "OutSum",
       invoice_id:  "InvId",
       signature:   "SignatureValue",
       email:       "Email",
       currency:    "IncCurrLabel",
       description: "Desc",
-      culture:     "Culture"
+      culture:     "Culture",
+      is_test:     "IsTest"
     }
 
     attr_accessor :invoice_id, :total, :params
@@ -28,13 +29,13 @@ module Rubykassa
     end
 
     def base_url
-      test_mode? ? "http://test.robokassa.ru/Index.aspx" : "https://merchant.roboxchange.com/Index.aspx"
+      test_mode? ? "https://auth.robokassa.ru/Merchant/Index.aspx" : "https://merchant.roboxchange.com/Index.aspx"
     end
 
     def pay_url(extra_params = {})
       extra_params = extra_params.slice(:currency, :description, :email, :culture)
 
-      "#{base_url}?" + initial_options.merge(extra_params).map do |key, value| 
+      "#{base_url}?" + initial_options.merge(extra_params).map do |key, value|
         if key =~ /^shp/
           "#{key}=#{value}"
         else
@@ -48,14 +49,15 @@ module Rubykassa
         login: Rubykassa.login,
         total: @total,
         invoice_id: @invoice_id,
+        is_test: test_mode? ? 1 : 0,
         signature: generate_signature_for(:payment)
       }.merge(Hash[@params.sort.map {|param_name| [param_name[0], param_name[1]]}])
     end
 
     private
 
-      def shpfy_params
-        @params = @params.map {|param_name| ["shp#{param_name[0]}".to_sym, param_name[1]]}
-      end
+    def shpfy_params
+      @params = @params.map {|param_name| ["shp#{param_name[0]}".to_sym, param_name[1]]}
+    end
   end
 end
